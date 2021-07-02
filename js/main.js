5279d15633c4f2c0bb0cdbbf74963b5700e05bb8
Тимофей <httimoha176@gmail.com>
2021-07-02 19:15:37 +0300

    load files working!

--- js/main.js
+++ js/main.js
@@ -1,23 +1,27 @@
-function main() {
-    const writeBookRadio = document.getElementById('writeBookRadio')
+const writeBookRadio = document.getElementById('writeBookRadio')
 const loadBookRadio = document.getElementById('loadBookRadio')
+
+writeBookRadio.addEventListener('change', function () {
+    if (this.checked) {
+        writeForm.style.display = 'block'
+        loadForm.style.display = 'none'
+    }
+})
+
+loadBookRadio.addEventListener('change', function () {
+    if (this.checked) {
+        loadForm.style.display = 'block'
+        writeForm.style.display = 'none'
+    }
+})
+
+function main() {
+
     const writeForm = document.querySelector('.writeForm')
     const bookList = document.querySelector('.list')
-    
-    
-    writeBookRadio.addEventListener('change', function () {
-        if (this.checked) {
-            writeForm.style.display = 'block'
-            loadForm.style.display = 'none'
-        }
-    })
-    
-    loadBookRadio.addEventListener('change', function () {
-        if (this.checked) {
-            loadForm.style.display = 'block'
-            writeForm.style.display = 'none'
-        }
-    })
+
+
+
 
     addButton.addEventListener('click', writeForm)
     bookList.addEventListener('click', function openBook(e) {
